a952276bf05e9d2ede6a73629a84fd0ae9aa5096
luki3k5 <luki3k5@gmail.com>
2013-10-31 11:59:35 +0100

    simplified code

--- lib/adswizz_api_wrapper/media_file.rb
+++ lib/adswizz_api_wrapper/media_file.rb
@@ -2,10 +2,14 @@ module AdswizzApiWrapper
   class MediaFile
     attr_accessor :url, :id, :type, :delivery, :bitrate, :width, :height
 
-    def initialize(media_file_details) # FIXME 
-      %w(url id type delivery bitrate width height).each do |v|
-        self.instance_variable_set("@#{v}", media_file_details.send(v.to_sym))
-      end
+    def initialize(media_file_details)
+      @url      = media_file_details.url
+      @id       = media_file_details.id
+      @type     = media_file_details.type
+      @delivery = media_file_details.delivery
+      @bitrate  = media_file_details.bitrate
+      @width    = media_file_details.width
+      @height   = media_file_details.height
     end
   end
 end
